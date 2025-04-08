3036100f3692ef4c91a0f3151187d1bc2729177b
mikheili zviadadze <72224442+zviadadze@users.noreply.github.com>
2025-04-08 11:45:45 +0400

    Remove extra folder

--- src/ninjas/ninjas.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { NinjasService } from './ninjas.service';
-import { NinjasController } from './ninjas.controller';
-
-@Module({
-  controllers: [NinjasController],
-  providers: [NinjasService],
-})
-export class NinjasModule {}
